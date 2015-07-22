48b2573e0804d8454d19a50065394cef2c3186cb
Damon Farquhar <zaider@gmail.com>
2015-07-21 22:26:04 -0400

    waits 10 seconds and then opens a web page

---
+++ TakeABreak2/TakeABreak2/TakeABreak2.py
@@ -0,0 +1,6 @@
+# run a timer that starts counting after 2 hours
+import webbrowser
+import time
+# do something with the timer control
+time.sleep(10)
+webbrowser.open("http://michaelcrump.net/setting-up-github-to-work-with-visual-studio-2013-step-by-step/")
